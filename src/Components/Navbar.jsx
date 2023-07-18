7561bcf24f115456cf6cbacd884260db3036d2ed
rishirathoree <rishirathoree3@gmail.com>
2023-07-18 14:49:43 +0530

    added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useRef } from 'react'
 import logo from '../assets/logo.png'
 import AccountPopUp from './AccountPopUp'
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
   return (
     <>
     <div className=' p-8 border-t-[6px] border-[#D54049] shadow-custom z-50 fixed w-full top-0 right-0 bg-white flex items-center justify-between'>
-        <div className='w-40'><img src={logo} alt="" /></div>
+        <Link to="/"><div className='w-40'><img src={logo} alt="" /></div></Link>
         <div className='flex items-center gap-4 '>
         <AccountPopUp />
         <span className='lg:hidden md:block sm:block' onClick={()=>{document.body.classList.toggle('sidebar-collapse')}}><i className='bx bx-menu-alt-right bx-sm'></i></span>
